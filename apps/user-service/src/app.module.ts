abeafba9425dea6fae24b96ff00dd85d87033538
Chinweike99 <innocentakwolu1@gmail.com>
2025-11-12 05:23:25 +0100

    feat: user module

--- apps/user-service/src/app.module.ts
+++ apps/user-service/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './config/ormconfig';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot(config),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -5,6 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 4000);
 
+  console.log('DB host:', process.env.DATABASE_HOST);
+console.log('DB user:', process.env.DATABASE_USER);
+
+
   console.log(`Server is running on http://localhost:${process.env.PORT ?? 4000}`);
 }
 bootstrap();
